58cbda686316dca10f70a3e98d2191dc4f4dd9d5
Achar, Suhas <Suhas.Achar@CenturyLink.com>
2021-05-21 20:26:50 +0530

    Parameterizing the Test

--- SampleService.Tests/SampleServiceShould.cs
+++ SampleService.Tests/SampleServiceShould.cs
@@ -5,16 +5,19 @@ namespace SampleService.Tests
     public class SampleServiceShould
     {
         [Test]
-        public void SquareTheInteger()
+        [TestCase(11)]
+        [TestCase(-7)]
+        [TestCase(145)]
+        public void SquareTheInteger(int n)
         {
             //  Arrange : Set the stage
             var service = new SampleService();
 
             //  Act : take the action
-            var square = service.Square(10);
+            var square = service.Square(n);
 
             //  Assert : test
-            Assert.AreEqual(10*10, square);
+            Assert.AreEqual(n*n, square);
         }
     }
 }

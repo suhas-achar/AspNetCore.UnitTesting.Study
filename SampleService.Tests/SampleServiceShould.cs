6c2184513a531a162879d0b0d040537100e0049f
Achar, Suhas <Suhas.Achar@CenturyLink.com>
2021-05-21 20:36:19 +0530

    Need for Setup

--- SampleService.Tests/SampleServiceShould.cs
+++ SampleService.Tests/SampleServiceShould.cs
@@ -4,68 +4,29 @@ namespace SampleService.Tests
 {
     public class SampleServiceShould
     {
-        //  Please refer: https://blog.johnwu.cc/images/a/243.png
-
-        public SampleServiceShould()
-        {
-            System.Console.WriteLine("Constructor");
-        }
-
-        [OneTimeSetUp]
-        public void BeforeAll()
-        {
-            System.Console.WriteLine("OneTimeSetUp");
-        }
+        private SampleService service;
 
         [SetUp]
         public void SetUp()
         {
-            System.Console.WriteLine("Setup");
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            System.Console.WriteLine("TearDown");
+            //  to avoid side-effects (from retained state in the object)
+            this.service = new SampleService();
         }
 
         [Test]
         public void SquareTheInteger()
         {
-            System.Console.WriteLine("SquareTheInteger");
-
-
-
-            //  Arrange : Set the stage
-            var service = new SampleService();
-
-            //  Act : take the action
             var square = service.Square(10);
 
-            //  Assert : test
             Assert.AreEqual(10*10, square);
         }
 
         [Test]
         public void ReturnMessage()
         {
-            System.Console.WriteLine("ReturnMessage");
-
-
-            //  Arrange : Set the stage
-            var service = new SampleService();
-
-            //  Act : take the action
             var message = service.GetMessage();
 
-            //  Assert : test
             Assert.AreEqual("Good morning", message);
-        }
-
-        [OneTimeTearDown]
-        public void AfterAll()
-        {
-            System.Console.WriteLine("OneTimeTearDown");
         }
     }
 }
